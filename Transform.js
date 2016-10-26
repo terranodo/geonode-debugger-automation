dc2760e501b58a56d9836d8245eb08fa4e9ffcdb
milafrerichs <mila.frerichs@gmail.com>
2016-10-26 17:16:46 +0200

    try to use boundless-sdk transpiled and not

---
+++ Transform.js
@@ -0,0 +1,187 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*
+ * Copyright 2015-present Boundless Spatial Inc., http://boundlessgeo.com
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+/**
+ * Transforms GXP style map config to our internal format.
+ */
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var MapConfigTransformService = (function () {
+  function MapConfigTransformService() {
+    _classCallCheck(this, MapConfigTransformService);
+  }
+
+  _createClass(MapConfigTransformService, [{
+    key: 'transform',
+    value: function transform(data, opt_proxy) {
+      var i,
+          ii,
+          layers = [];
+      var groups = {};
+      for (i = 0, ii = data.map.layers.length; i < ii; ++i) {
+        var layer = data.map.layers[i];
+        var source = data.sources[layer.source];
+        var url = source.url;
+        if (opt_proxy) {
+          url = opt_proxy + source.url;
+        }
+        var layerConfig = {
+          properties: {
+            isRemovable: true,
+            visible: layer.visibility,
+            title: layer.title || layer.name.split(':').pop(),
+            id: layer.name,
+            name: layer.name
+          }
+        };
+        if (source.ptype === 'gxp_osmsource') {
+          layerConfig.type = 'Tile';
+          layerConfig.source = {
+            type: 'OSM',
+            properties: {
+              crossOrigin: 'anonymous'
+            }
+          };
+        } else if (source.ptype === 'gxp_arcrestsource') {
+          layerConfig.type = 'Tile';
+          layerConfig.source = {
+            type: 'TileArcGISRest',
+            properties: {
+              crossOrigin: 'anonymous',
+              urls: [url],
+              params: {
+                LAYERS: layer.layerid,
+                FORMAT: layer.format
+              }
+            }
+          };
+        } else if (source.ptype === 'gxp_wmscsource') {
+          layerConfig.properties.popupInfo = '#AllAttributes';
+          if (layer.capability) {
+            layerConfig.properties.isSelectable = layer.capability.queryable;
+            layerConfig.properties.isWFST = layer.capability.queryable;
+            layerConfig.properties.styleName = layer.capability.styles[0].name;
+            layerConfig.properties.legendUrl = layer.capability.styles[0].legend.href;
+            layerConfig.properties.EX_GeographicBoundingBox = layer.capability.llbbox;
+          }
+          layerConfig.type = 'Tile';
+          var params = {
+            LAYERS: layer.name,
+            TILED: 'TRUE'
+          };
+          if (layer.styles) {
+            params.STYLES = layer.styles;
+          }
+          if (layer.format) {
+            params.FORMAT = layer.format;
+          }
+          if (layer.transparent !== undefined) {
+            params.TRANSPARENT = layer.transparent;
+          }
+          layerConfig.source = {
+            type: 'TileWMS',
+            properties: {
+              crossOrigin: 'anonymous',
+              params: params,
+              url: url
+            }
+          };
+        } else if (source.ptype === 'gxp_mapboxsource') {
+          var urls = ['http://a.tiles.mapbox.com/v1/mapbox.' + layer.name + '/', 'http://b.tiles.mapbox.com/v1/mapbox.' + layer.name + '/', 'http://c.tiles.mapbox.com/v1/mapbox.' + layer.name + '/', 'http://d.tiles.mapbox.com/v1/mapbox.' + layer.name + '/'];
+          var attribution = /^world/.test(layer.name) ? '<a href="http://mapbox.com">MapBox</a> | Some Data &copy; OSM CC-BY-SA | <a href="http://mapbox.com/tos">Terms of Service</a>' : '<a href="http://mapbox.com">MapBox</a> | <a href="http://mapbox.com/tos">Terms of Service</a>';
+          var maxZoom = {
+            'blue-marble-topo-bathy-jan': 8,
+            'blue-marble-topo-bathy-jul': 8,
+            'blue-marble-topo-jan': 8,
+            'blue-marble-topo-jul': 8,
+            'control-room': 8,
+            'geography-class': 8,
+            'natural-earth-hypso': 6,
+            'natural-earth-hypso-bathy': 6,
+            'natural-earth-1': 6,
+            'natural-earth-2': 6,
+            'world-dark': 11,
+            'world-light': 11,
+            'world-glass': 10,
+            'world-print': 9
+          };
+          layerConfig.type = 'Tile';
+          layerConfig.source = {
+            type: 'TMS',
+            properties: {
+              attributions: [attribution],
+              format: 'png',
+              urls: urls,
+              maxZoom: maxZoom[layer.name]
+            }
+          };
+        } else if (source.ptype === 'gxp_bingsource') {
+          layerConfig.type = 'Tile';
+          layerConfig.source = {
+            type: 'BingMaps',
+            properties: {
+              key: source.apiKey,
+              imagerySet: layer.name
+            }
+          };
+        } else {
+          layerConfig = undefined;
+        }
+        if (layerConfig !== undefined) {
+          if (layer.group) {
+            if (layer.group === 'background') {
+              layerConfig.properties.type = 'base';
+            }
+            if (!groups[layer.group]) {
+              groups[layer.group] = {
+                type: 'Group',
+                properties: {
+                  title: layer.group === 'background' ? 'Base Maps' : layer.group,
+                  type: layer.group === 'background' ? 'base-group' : undefined
+                },
+                children: []
+              };
+              layers.push(groups[layer.group]);
+            }
+            groups[layer.group].children.push(layerConfig);
+          } else {
+            layers.push(layerConfig);
+          }
+        }
+      }
+      return {
+        layers: layers,
+        view: {
+          center: data.map.center,
+          projection: data.map.projection,
+          zoom: data.map.zoom
+        }
+      };
+    }
+  }]);
+
+  return MapConfigTransformService;
+})();
+
+exports['default'] = new MapConfigTransformService();
+module.exports = exports['default'];
+
+},{}]},{},[1]);
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const Promise = require('promise');
 //
-//MapConfigTransformService = require('boundless-sdk/js/services/MapConfigTransformService.js');
+MapConfigTransformService = require('./MapConfigTransformService1.js');
 //
 let sources = [];
 const fetchConfigFromUrl = (url) => {
@@ -14,8 +14,8 @@ const fetchConfigFromUrl = (url) => {
       for (var key of Object.keys(json.sources)) {
         var source = json.sources[key];
         const config = createMinimalConfig(source);
-        var result = { 'layers': [] }; //result from MapConfigTransformService.transform(testConfig);
-        if(result.layers.length !== 1) { //test with sdk
+        var result = MapConfigTransformService.transform(config);
+        if(result.layers.length !== 1) {
           console.log('Source type '+source.ptype+' cant be created');
           if(sources.indexOf(source.ptype) === -1) {
             sources.push(source.ptype);
@@ -31,13 +31,6 @@ const createMinimalConfig = (source) => {
   return minimalConfig;
 };
 const urls = ['http://geosm.adpc.net/maps/193/data', 'http://geosm.adpc.net/maps/193/data'];
-/*
-urls.forEach( (value) => {
-  promiseConfig(value).then((done) => {
-    console.log(sources);
-  });
-});
-*/
 const promises = urls.map( (url) => {
   return fetchConfigFromUrl(url)
 });
