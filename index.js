82bd7c279e1dd9d6be232f4c18e363aaba5a300c
milafrerichs <mila.frerichs@gmail.com>
2016-10-27 15:59:53 +0200

    Parse the geonode endpoints for the maps
    test every map for that geonode and put the onces that did not return a
    layer into a csv

--- index.js
+++ index.js
@@ -4,37 +4,72 @@ const MapConfigTransformService = require('./MapConfigTransformService.js');
 const csvWriter = require('csv-write-stream');
 var fs = require('fs');
 
-let sources = [];
+let distinctSources = [];
 let configs = [];
 let dataSources = [];
-const writeToCSV = (data, headers) => {
+let numberOfSources = 0;
+const writeToCSV = (data, headers, fileName) => {
   var writer = csvWriter({ headers: headers });
-  writer.pipe(fs.createWriteStream('./sources.csv'));
+  writer.pipe(fs.createWriteStream('./'+fileName));
   data.forEach( (d) => {
     writer.write(d);
   })
   writer.end();
 }
+const fetchMapsFromUrl = (url) => {
+  return fetch(url+'/api/maps').then((response) => {
+    if(response.status == 200) {
+      return response.json();
+    }else {
+      console.log('status: ', response);
+    }
+  }).then((json) => {
+    if(json) {
+      return Promise.all(json.objects.map(function (object) {
+        if(object.detail_url) {
+          return url+object.detail_url+'/data';
+        }
+      }));
+    }else {
+      console.log('no json: ');
+    }
+  }).catch((error) => {
+    console.log('error: ',error);
+  });
+};
 const fetchConfigFromUrl = (url) => {
   return fetch(url).then((response) => {
     if(response.status == 200) {
       return response.json();
+    }else {
+      console.log('status: ', response);
     }
   }).then((json) => {
     if(json) {
       for (var key of Object.keys(json.sources)) {
         var source = json.sources[key];
-        const config = createMinimalConfig(source);
+        let config = createMinimalConfig(source);
         var result = MapConfigTransformService.transform(config);
         if(result.layers.length !== 1) {
           configs.push({url: url, source: source});
           dataSources.push({url: url, type: source.ptype, geonode: source.url});
-          if(sources.indexOf(source.ptype) === -1) {
-            sources.push(source.ptype);
+          if(distinctSources.indexOf(source.ptype) === -1) {
+            console.log('push source type: ', source.ptype);
+            distinctSources.push(source.ptype);
           }
         }
       }
+      const sourceLength = Object.keys(json.sources).length;
+      if(sourceLength) {
+        numberOfSources += sourceLength;
+        return sourceLength;
+      } else {
+      }
+    } else {
+      console.log('no json', json);
     }
+  }).catch((error) => {
+    console.log('error: ',error);
   });
 };
 const createMinimalConfig = (source) => {
@@ -42,13 +77,25 @@ const createMinimalConfig = (source) => {
   minimalConfig.sources = { '1': source };
   return minimalConfig;
 };
-const urls = ['http://geosm.adpc.net/maps/193/data', 'http://geosm.adpc.net/maps/193/data'];
-const promises = urls.map( (url) => {
-  return fetchConfigFromUrl(url)
+const geonodes = ['http://zansea-geonode.org','http://ebolageonode.org', 'http://demo.geonode.org', 'http://geosm.adpc.net', 'http://secondarycities.geonode.state.gov', 'http://data.adriplan.eu',];
+const geonodeMapsPromise = geonodes.map( (url) => {
+  return fetchMapsFromUrl(url);
 });
-Promise.all(promises).then( (done) => {
-  console.log('Sources: ',sources);
-  console.log('Configs: ',configs);
-  console.log('Sources: ',dataSources);
-  writeToCSV(dataSources, ['url', 'type', 'geonode']);
+Promise.all(geonodeMapsPromise).then( (urls) => {
+  return Promise.all(urls.map((geonodeUrls) => {
+    return Promise.all(geonodeUrls.map( (url) => {
+      return fetchConfigFromUrl(url);
+    }));
+  }));
+}).then( (done) => {
+  console.log('done', numberOfSources);
+  console.log('sources', distinctSources);
+  writeToCSV(dataSources, ['url', 'type', 'geonode'], "sources_long.csv");
+  console.log('sources', distinctSources);
+  var writer = csvWriter({headers: ['type']});
+  writer.pipe(fs.createWriteStream('./sources_distinct.csv'));
+  distinctSources.forEach( (d) => {
+    writer.write([d]);
+  })
+  writer.end();
 });
